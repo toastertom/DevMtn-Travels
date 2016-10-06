d871ea1263b9c6399b190c062ff06d86034ceaa2
ToasterTom <tate.thomas5595@gmail.com>
2016-10-05 20:56:41 -0600

    Booking is picking up URL id

--- js/controllers/bookedCtrl.js
+++ js/controllers/bookedCtrl.js
@@ -1,7 +1,14 @@
-angular.module('devmtnTravel').controller('bookedCtrl', function ($scope, $state, mainSrv) {
+angular.module('devmtnTravel').controller('bookedCtrl', function ($scope, $stateParams, mainSrv) {
 
-  if ($stateParams.id === 'shoes') {
-        $scope.productData = productService.shoeData;
-    }
+  $scope.location = mainSrv.getLocation($stateParams.id);
 
-})
+  // getLocation();
+  // function getLocation() {
+  //   let id = $stateParams.id;
+  //   $scope.location = mainSrv.getLocation(id);
+  // }
+});
+
+
+// you will need to write a function that checks the url params and then loops over the data object in the service and then returns
+// the object the matches the id being passed in the url params. Do this in your controller -->
